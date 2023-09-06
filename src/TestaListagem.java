979eae1d6bc32bca8c10e02900cae31343b84769
Flávio Félix <fau_felix30@hotmail.com>
2023-09-06 19:50:22 -0300

    Listagem com Statement

--- src/TestaListagem.java
+++ src/TestaListagem.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -12,7 +13,19 @@ public class TestaListagem {
 		System.out.println("Fechando a conexão!!");
 		
 		Statement stm = con.createStatement();
-		stm.execute("SELECT ID, NOME, DESCRICAO FROM PRODUTO");
+		
+		 stm.execute("SELECT ID, NOME, DESCRICAO FROM PRODUTO");
+		 
+		 ResultSet  rst = stm.getResultSet();
+		
+		while (rst.next()) {
+			Integer id = rst.getInt("ID");
+			System.out.println(id);
+			String nome = rst.getString("NOME");
+			System.out.println(nome);
+			String descricao = rst.getString("DESCRICAO");
+			System.out.println(descricao);
+		}
 		
 		con.close();
 
