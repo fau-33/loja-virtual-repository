c55a0b7cd9461ea78c4a68a3b6e300441d8a9b76
Flávio Félix <fau_felix30@hotmail.com>
2023-09-09 14:26:41 -0300

    Assumindo controle

--- src/TestaInsercaoComParametro.java
+++ src/TestaInsercaoComParametro.java
@@ -8,20 +8,27 @@ public class TestaInsercaoComParametro {
 
 	public static void main(String[] args) throws SQLException {
 		
-		String nome = "Mouse'";
-		String descricao = "Mouse sem fio); delete from PRODUTO";
-		
 		ConnectionFactory factory = new ConnectionFactory();
 		Connection connection = factory.recuperarConexao();
-		
+		connection.setAutoCommit(false);
 		
 		
 		PreparedStatement stm = connection
 				.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)", Statement.RETURN_GENERATED_KEYS);	
 		
 		
+		adicionarVariavel("SmarTV", "45 polegadas", stm);
+		adicionarVariavel("Radio", "Radio de bateria", stm);
+	}
+
+	private static void adicionarVariavel(String nome, String descricao, PreparedStatement stm) throws SQLException {
 		stm.setString(1, nome);
 		stm.setString(2, descricao);
+		
+//		if(nome.equals("Radio")) {
+//			throw new RuntimeException("Não foi possivel adicionar o produto");
+//		}
+		
 		stm.execute();
 		
 		ResultSet rst = stm.getGeneratedKeys();
@@ -30,6 +37,7 @@ public class TestaInsercaoComParametro {
 			Integer id = rst.getInt(1);
 			System.out.println("O id criado foi " + id);
 		}
+		rst.close();
 	}
 
 }
