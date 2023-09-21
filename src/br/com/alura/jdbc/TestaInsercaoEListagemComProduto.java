6907904b9e9a2438eacb5b5b3c91bd0e433629cd
Flávio Félix <fau_felix30@hotmail.com>
2023-09-21 14:00:33 -0300

    Listando no ProdutoDAO

--- src/br/com/alura/jdbc/TestaInsercaoEListagemComProduto.java
+++ src/br/com/alura/jdbc/TestaInsercaoEListagemComProduto.java
@@ -1,14 +1,16 @@
+package br.com.alura.jdbc;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
 
 import br.com.alura.jdbc.ConnectionFactory;
 import br.com.alura.jdbc.dao.ProdutoDAO;
 import br.com.alura.jdbc.modelo.Produto;
 
-public class TestaInsercaoComProduto {
+public class TestaInsercaoEListagemComProduto {
 	public static void main(String[] args) throws SQLException {
 		
 		Produto comoda = new Produto("Cômoda", "Cômoda Vertical");
@@ -17,7 +19,8 @@ public class TestaInsercaoComProduto {
 			ProdutoDAO produtoDAO = new ProdutoDAO(connection);
 			
 			produtoDAO.salvar(comoda);
-			// Lista = persistenciaProduto.listar();
+			List<Produto> listaDeProdutos = produtoDAO.listar();
+			listaDeProdutos.stream().forEach(lp -> System.out.println(lp));
 		}
 		
 	}
